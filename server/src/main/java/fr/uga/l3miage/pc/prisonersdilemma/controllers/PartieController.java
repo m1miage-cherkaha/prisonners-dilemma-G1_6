221d1fed2fb6768f320c9daa0a753e473615ec87
thauvinl <lola.tosetto@etu.univ-grenoble-alpes.fr>
2024-12-11 22:35:38 +0100

    refactored the requests mapping to the controller (instead of the endpoint)

--- server/src/main/java/fr/uga/l3miage/pc/prisonersdilemma/controllers/PartieController.java
+++ server/src/main/java/fr/uga/l3miage/pc/prisonersdilemma/controllers/PartieController.java
@@ -2,17 +2,25 @@ package fr.uga.l3miage.pc.prisonersdilemma.controllers;
 
 
 import fr.uga.l3miage.pc.prisonersdilemma.services.PartieService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import fr.uga.l3miage.pc.prisonersdilemma.endpoints.PartieEndpoint;
 import fr.uga.l3miage.pc.prisonersdilemma.requests.PartieCreationRequest;
 import fr.uga.l3miage.pc.prisonersdilemma.requests.PartieJoinRequest;
 import fr.uga.l3miage.pc.prisonersdilemma.responses.PartieResponseDTO;
 import fr.uga.l3miage.pc.prisonersdilemma.responses.ScoreResponseDTO;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+
+@RestController
+@RequestMapping("/api/parties")
 public class PartieController implements PartieEndpoint {
     private final PartieService partieService;
 
+    public PartieController(PartieService partieService) {
+        this.partieService = partieService;
+    }
    @Override
    public PartieResponseDTO createPartie(PartieCreationRequest partieCreationRequest){
         return partieService.demarrerNouvellePartie(partieCreationRequest);
--- server/src/main/java/fr/uga/l3miage/pc/prisonersdilemma/controllers/TourController.java
+++ server/src/main/java/fr/uga/l3miage/pc/prisonersdilemma/controllers/TourController.java
@@ -1,15 +1,23 @@
 package fr.uga.l3miage.pc.prisonersdilemma.controllers;
 
 import fr.uga.l3miage.pc.prisonersdilemma.services.PartieService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import fr.uga.l3miage.pc.prisonersdilemma.endpoints.TourEndpoints;
 import fr.uga.l3miage.pc.prisonersdilemma.requests.TourRequestDTO;
 import fr.uga.l3miage.pc.prisonersdilemma.responses.TourResponseDTO;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+
+@RestController
+@RequestMapping("/api/tours")
 public class TourController implements TourEndpoints {
     private final PartieService tourService;
     
+    public TourController(PartieService tourService) {
+        this.tourService = tourService;
+    }
     @Override
     public TourResponseDTO postFaireChoix(TourRequestDTO tour) {
         return tourService.jouerTour(tour);
